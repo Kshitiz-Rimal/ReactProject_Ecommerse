370d909acec43e364811d868448a984dbad87d08
Kshitiz_Rimal <kshitizrimal123@gmail.com>
2022-08-15 12:14:31 +0545

    Text area added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
+import TextArea from "./Components/TextArea";
 
 function App() {
   return (
     <>
+      <TextArea />
       <Navbar/>
     </>
   );
