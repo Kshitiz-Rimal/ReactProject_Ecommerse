618f37d084981081c62353116af8fa2f4508a41f
Kshitiz_Rimal <kshitizrimal123@gmail.com>
2022-08-18 12:43:14 +0545

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,30 +1,42 @@
 import "./App.css";
-import React,{useState} from "react";
+import React, { useState } from "react";
 import Navbar from "./Components/Navbar";
 import TextArea from "./Components/TextArea";
 import About from "./Components/About";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
-  const [mode, setMode] = useState('light')
+  const [mode, setMode] = useState("light");
 
-  const toggleMode=()=>{
-    if (mode==='light'){
-      setMode('dark');
-      document.body.style.backgroundColor="black"
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "black";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
     }
-    else{
-      setMode('light');
-      document.body.style.backgroundColor="white"
-
-    }
-  }
+  };
   return (
     <>
-      <Navbar mode={mode} toggleMode={toggleMode} />
-      <div className="container my-3">
-        <TextArea heading="Enter Text to analyse" mode={mode}/>
-        <About mode={mode}/>
-      </div>
+      <Router>
+        <Navbar mode={mode} toggleMode={toggleMode} />
+        <div className="container my-3">
+          <Routes>
+            <Route exact path="/about" element={<About mode={mode} />}/>
+              
+            
+            <Route exact path="/" element={<TextArea heading="Enter Text to analyse" mode={mode} />}/>
+
+          </Routes>
+        </div>
+      </Router>
+
+      {/* Original without route */}
+      {/* <Navbar mode={mode} toggleMode={toggleMode} />
+      <div className="container my-3"></div>
+      <TextArea heading="Enter Text to analyse" mode={mode} />
+      <About mode={mode} /> */}
     </>
   );
 }
